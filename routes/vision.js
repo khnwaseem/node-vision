750fa2ea7c4fb2313be13b4e33ea611f30979fc2
khnwaseem <khanwaseem094@gmail.com>
2022-01-22 22:23:14 +0530

    completed project

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+require('dotenv').config()
+
 
 router.post('/classify', function(req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
 
   // Your code starts here //
 
-  // Your code ends here //
+AWS.config.update({region:process.env.AWS_REGION,
+accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
 
-  res.json({
-    "labels": response
+const client = new AWS.Rekognition();
+var imageData = Buffer.from(req.files.file.data, 'base64');
+
+  const params = {
+    Image: {
+          Bytes: imageData
+        },
+      "MaxLabels": 10
+  }
+
+  client.detectLabels(params, function(err, res) {
+    if (err) {
+      console.log(err, err.stack); // if an error occurred
+    } else {
+      let response = [];
+
+      res.Labels.forEach(label => {
+        response.push(label.Name)
+        console.log(`Label:      ${label.Name}`)
+
+      }) // for response.labels
+      DetectLabel(response)
+
+    } // if
   });
+
+
+  // Your code ends here //
+  
+const DetectLabel = (response) => {
+  res.json({
+    "labels": response,
+  });
+}
+
 });
 
 module.exports = router;
